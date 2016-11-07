f4602c9bfd52c23a36961040ab2dad84f0848b47
Alexandre Roux Tekartik <alex@tekartik.com>
2016-11-07 11:58:22 +0100

    feat: add layout experiment

---
+++ example/side_bar_layout/main.dart
@@ -0,0 +1,26 @@
+import 'package:tekartik_browser_utils/browser_utils_import.dart';
+import 'package:tekartik_app_utils/side_bar_layout/loader.dart';
+import 'package:tekartik_bootstrap/bootstrap_loader.dart';
+import 'package:tekartik_app_utils/side_bar_layout/side_bar_layout.dart';
+
+main() async {
+  devPrint('hi');
+  await loadBootstrap();
+  await loadSideBarCss();
+  SideBarLayout sideBarLayout = new SideBarLayout();
+  querySelector("#menu-toggle").onClick.listen((e) {
+    e.preventDefault();
+    //querySelector("#wrapper").classes.toggle("toggled");
+    sideBarLayout.toggle();
+  });
+  querySelector("#menu-show").onClick.listen((e) {
+    e.preventDefault();
+    //querySelector("#wrapper").classes.add("show");
+    sideBarLayout.showMenu();
+  });
+  querySelector("#menu-reset").onClick.listen((e) {
+    e.preventDefault();
+    sideBarLayout.reset();
+    //querySelector("#wrapper").classes.add("show");
+  });
+}
---
+++ lib/side_bar_layout/side_bar_layout.dart
@@ -0,0 +1,33 @@
+import 'dart:html';
+
+class SideBarLayout {
+  Element _element;
+
+  SideBarLayout([Element element]) {
+    if (element == null) {
+      element = querySelector("#wrapper");
+    }
+    _element = element;
+  }
+  void showMenu() {
+    //$("#menu-toggle").click(function(e) {
+    //e.preventDefault();
+    _element.classes.remove("toggled");
+    _element.classes.add("show"); //toggleClass("toggled");
+  }
+  void reset() {
+    //$("#menu-toggle").click(function(e) {
+    //e.preventDefault();
+    _element.classes.remove("toggled");
+    _element.classes.remove("show");
+    //querySelector("#wrapper").classes.remove("toggled"); //toggleClass("toggled");
+  }
+
+  void toggle() {
+    //$("#menu-toggle").click(function(e) {
+    //e.preventDefault();
+    _element.classes.remove("show");
+    _element.classes.toggle("toggled");
+    //querySelector("#wrapper").classes.remove("toggled"); //toggleClass("toggled");
+  }
+}
