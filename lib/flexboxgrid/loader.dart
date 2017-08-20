bd2fdd6f6e0851c3b66c3a74a8c69c0038f3ff82
Alexandre Roux Tekartik <alex@tekartik.com>
2017-08-20 20:02:02 +0200

    feat: add flexbox grid hide (untested)

--- lib/flexboxgrid/loader.dart
+++ lib/flexboxgrid/loader.dart
@@ -1,7 +1,20 @@
 import 'package:tekartik_browser_utils/css_utils.dart';
 import 'dart:async';
+import 'package:tekartik_common_utils/async_utils.dart';
 
-Future loadFlexBoxGridCss() async {
-  await loadStylesheet(
-      "packages/tekartik_app_utils/flexboxgrid/flexboxgrid.min.css");
-}
+Future loadFlexBoxGridPlusCss() => _plusCssLoader.run();
+
+StylesheetLoader _cssLoader = new StylesheetLoader(
+    "packages/tekartik_app_utils/flexboxgrid/flexboxgrid.min.css");
+
+Future loadFlexBoxGridCss() => _cssLoader.load();
+
+StylesheetLoader _hideCssLoader = new StylesheetLoader(
+    "packages/tekartik_app_utils/flexboxgrid/flexboxgrid_hide.css");
+
+Future loadFlexBoxGridHideCss() => _hideCssLoader.load();
+
+AsyncOnceRunner _plusCssLoader = new AsyncOnceRunner(() async {
+  await loadFlexBoxGridCss();
+  await loadFlexBoxGridHideCss();
+});
