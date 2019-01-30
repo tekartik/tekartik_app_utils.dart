1cd5815d53dc508d962c1e725120372369521cc7
alex <alex@tekartik.com>
2019-01-30 09:59:42 +0100

    fix: recaptcha V3 support

--- example/recaptcha/v3/main.dart
+++ example/recaptcha/v3/main.dart
@@ -5,11 +5,9 @@ import 'package:tekartik_app_utils/recaptcha/recaptcha.dart';
 import 'package:tekartik_browser_utils/browser_utils_import.dart';
 
 Future main() async {
-  // querySelector('#output').text = 'Your Dart app is running.';
   var siteKey = '6LdXqY0UAAAAADHEJ-K2IiDsDBkw8f15qg3WORJj';
   PreOutput output = PreOutput(id: 'output');
   output.writeln('waiting for non-robot');
-  await grecaptchaLoadJs();
   var token = await grecaptchaV3Wait(siteKey: siteKey, action: 'test');
   output.writeln('token: $token');
 }
