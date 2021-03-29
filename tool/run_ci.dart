ccf8d78b80891ae23d3b435674d25cefad72a940
alex <alex@tekartik.com>
2021-03-29 12:42:47 +0200

    fix github actions

---
+++ tool/run_ci.dart
@@ -0,0 +1,6 @@
+import 'package:dev_test/package.dart';
+import 'package:process_run/shell.dart';
+
+Future main() async {
+  await packageRunCi('.');
+}
--- tool/travis.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dev_test/package.dart';
-import 'package:process_run/shell.dart';
-
-Future main() async {
-  await packageRunCi('.', noAnalyze: true);
-  await Shell().run('dart analyze --fatal-warnings .'); // no --fatal-infos
-}
